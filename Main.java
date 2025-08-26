8d0190b96b53904ee94f84a0086f81ae4d9c5ab8
Mohammad Raza <165828987+mohammadrrazaa@users.noreply.github.com>
2025-08-26 17:37:03 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package try1;
+
+public class Main{
+
+    public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("System is loading...........");
+		System.out.println("welcome to csc229 fall 2024 term");
+	}
+}
